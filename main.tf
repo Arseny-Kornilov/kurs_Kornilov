35aa72f15e2a5aea3e551ef6f0ac844054cd371d
Arseny Kornilov <arsenykornilov3345@gmail.com>
2026-01-19 13:57:09 +0000

    Completed but not checked

--- main.tf
+++ main.tf
@@ -27,14 +27,232 @@ resource "yandex_vpc_subnet" "foo3" {
   v4_cidr_blocks = ["192.168.1.0/24"]
 }
 
-
-# Security Group 1 - Web Servers
-
-resource "yandex_vpc_security_group" "web_servers_sg" {
-  name        = "web-servers-sg"
-  description = "Security group for web servers (nginx)"
+# Security Group для внутренней сети
+resource "yandex_vpc_security_group" "private_network_sg" {
+  name        = "private-network-sg"
+  description = "Allow all traffic within private subnet"
   network_id  = yandex_vpc_network.main.id
 
+
+  ingress {
+    description    = "Allow all inside private subnet"
+    protocol       = "ANY"
+    v4_cidr_blocks = [yandex_vpc_subnet.private.v4_cidr_blocks[0]]
+  }
+
+  ingress {
+    description       = "Allow SSH only from Bastion host"
+    protocol          = "TCP"
+    security_group_id = yandex_vpc_security_group.bastion_sg.id # ← Ключевое!
+    port              = 22
+  }
+
+  egress {
+    description    = "Allow all outbound"
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "yandex_vpc_security_group" "bastion_sg" {
+  name        = "bastion-sg"
+  description = "Security group for Bastion host - SSH only"
+  network_id  = yandex_vpc_network.main.id
+
+  # ТОЛЬКО SSH из интернета
+  ingress {
+    description    = "Allow SSH from anywhere (или ограничьте свой IP)"
+    protocol       = "TCP"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 22
+  }
+
+  # Разрешить SSH к приватным хостам
+  egress {
+    description    = "Allow SSH to private subnet"
+    protocol       = "TCP"
+    v4_cidr_blocks = [yandex_vpc_subnet.private.v4_cidr_blocks[0]]
+    port           = 22
+  }
+
+  # Для обновлений пакетов
+  egress {
+    description    = "Allow HTTPS for updates"
+    protocol       = "TCP"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 443
+  }
+}
+
+#Security Web Servers
+resource "yandex_vpc_security_group" "web_servers_sg" {
+  name        = "web-servers-sg"
+  description = "Security group for web servers in private subnet"
+  network_id  = yandex_vpc_network.main.id
+
+  # HTTP/HTTPS от ALB (публичная подсеть)
+  ingress {
+    description    = "Allow HTTP from Application Load Balancer"
+    protocol       = "TCP"
+    v4_cidr_blocks = [yandex_vpc_subnet.public.v4_cidr_blocks[0]] # Публичная подсеть ALB
+    port           = 80
+  }
+
+  # SSH только из приватной подсети (для администрирования)
+  ingress {
+    description    = "Allow SSH from private subnet"
+    protocol       = "TCP"
+    v4_cidr_blocks = [yandex_vpc_subnet.private.v4_cidr_blocks[0]]
+    port           = 22
+  }
+
+  # Метрики для Prometheus (из приватной подсети)
+  ingress {
+    description    = "Allow Node Exporter from monitoring subnet"
+    protocol       = "TCP"
+    v4_cidr_blocks = [yandex_vpc_subnet.private.v4_cidr_blocks[0]] # Prometheus тоже в приватной
+    port           = 9100
+  }
+
+  ingress {
+    description    = "Allow Nginx Log Exporter from monitoring subnet"
+    protocol       = "TCP"
+    v4_cidr_blocks = [yandex_vpc_subnet.private.v4_cidr_blocks[0]]
+    port           = 4040
+  }
+
+  # Отправка логов в Elasticsearch (приватная подсеть)
+  egress {
+    description    = "Allow sending logs to Elasticsearch"
+    protocol       = "TCP"
+    v4_cidr_blocks = [yandex_vpc_subnet.private.v4_cidr_blocks[0]] # Elasticsearch в приватной
+    port           = 9200
+  }
+}
+
+#Security Group Prometheus
+resource "yandex_vpc_security_group" "prometheus_sg" {
+  name        = "prometheus-sg"
+  description = "Security group for Prometheus in private subnet"
+  network_id  = yandex_vpc_network.main.id
+
+  # Веб-интерфейс Prometheus - доступ из приватной подсети
+  ingress {
+    description    = "Allow Prometheus UI from private subnet"
+    protocol       = "TCP"
+    v4_cidr_blocks = [yandex_vpc_subnet.private.v4_cidr_blocks[0]]
+    port           = 9090
+  }
+
+  # Сбор метрик с веб-серверов (приватная подсеть)
+  egress {
+    description    = "Allow to scrape web servers"
+    protocol       = "TCP"
+    v4_cidr_blocks = [yandex_vpc_subnet.private.v4_cidr_blocks[0]]
+    port           = 9100
+  }
+
+  egress {
+    description    = "Allow to scrape nginx logs"
+    protocol       = "TCP"
+    v4_cidr_blocks = [yandex_vpc_subnet.private.v4_cidr_blocks[0]]
+    port           = 4040
+  }
+}
+
+# Security group Elasticsearch
+resource "yandex_vpc_security_group" "elasticsearch_sg" {
+  name        = "elasticsearch-sg"
+  description = "Security group for Elasticsearch in private subnet"
+  network_id  = yandex_vpc_network.main.id
+
+  # API для Filebeat (из приватной подсети)
+  ingress {
+    description    = "Allow Elasticsearch API from private subnet"
+    protocol       = "TCP"
+    v4_cidr_blocks = [yandex_vpc_subnet.private.v4_cidr_blocks[0]] # Веб-серверы в приватной
+    port           = 9200
+  }
+
+  # Для Kibana (публичная подсеть)
+  ingress {
+    description    = "Allow Kibana access from public subnet"
+    protocol       = "TCP"
+    v4_cidr_blocks = [yandex_vpc_subnet.public.v4_cidr_blocks[0]]
+    port           = 9200
+  }
+
+}
+
+#Security group Grafana
+resource "yandex_vpc_security_group" "grafana_sg" {
+  name        = "grafana-sg"
+  description = "Security group for Grafana in public subnet"
+  network_id  = yandex_vpc_network.main.id
+
+  # Веб-интерфейс Grafana для всех
+  ingress {
+    description    = "Allow Grafana UI from anywhere"
+    protocol       = "TCP"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 3000
+  }
+
+  # SSH только из приватной подсети (или ваш IP)
+  ingress {
+    description       = "Allow SSH from Bastion"
+    protocol          = "TCP"
+    security_group_id = yandex_vpc_security_group.bastion_sg.id
+    port              = 22
+  }
+
+  # Доступ к Prometheus (приватная подсеть)
+  egress {
+    description    = "Allow connection to Prometheus"
+    protocol       = "TCP"
+    v4_cidr_blocks = [yandex_vpc_subnet.private.v4_cidr_blocks[0]]
+    port           = 9090
+  }
+}
+
+#Security Group Kibana
+resource "yandex_vpc_security_group" "kibana_sg" {
+  name        = "kibana-sg"
+  description = "Security group for Kibana in public subnet"
+  network_id  = yandex_vpc_network.main.id
+
+  # Веб-интерфейс Kibana для всех
+  ingress {
+    description    = "Allow Kibana UI from anywhere"
+    protocol       = "TCP"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 5601
+  }
+
+  # SSH только из приватной подсети
+  ingress {
+    description       = "Allow SSH from Bastion"
+    protocol          = "TCP"
+    security_group_id = yandex_vpc_security_group.bastion_sg.id
+    port              = 22
+  }
+
+  # Доступ к Elasticsearch (приватная подсеть)
+  egress {
+    description    = "Allow connection to Elasticsearch"
+    protocol       = "TCP"
+    v4_cidr_blocks = [yandex_vpc_subnet.private.v4_cidr_blocks[0]]
+    port           = 9200
+  }
+}
+
+# Security group Application Load Balancer
+resource "yandex_vpc_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Security group for Application Load Balancer"
+  network_id  = yandex_vpc_network.main.id
+
+  # HTTP для всех
   ingress {
     description    = "Allow HTTP from anywhere"
     protocol       = "TCP"
@@ -42,6 +260,7 @@ resource "yandex_vpc_security_group" "web_servers_sg" {
     port           = 80
   }
 
+  # HTTPS для всех (опционально)
   ingress {
     description    = "Allow HTTPS from anywhere"
     protocol       = "TCP"
@@ -49,31 +268,12 @@ resource "yandex_vpc_security_group" "web_servers_sg" {
     port           = 443
   }
 
-  ingress {
-    description    = "Allow SSH from trusted IPs"
-    protocol       = "TCP"
-    v4_cidr_blocks = ["your.trusted.ip/32"]  # Только ваш IP!
-    port           = 22
-  }
-
-  ingress {
-    description    = "Allow Node Exporter from Prometheus"
-    protocol       = "TCP"
-    v4_cidr_blocks = ["10.128.0.12/32"]  # Только Prometheus (vm3)
-    port           = 9100
-  }
-
-  ingress {
-    description    = "Allow Nginx Log Exporter from Prometheus"
-    protocol       = "TCP"
-    v4_cidr_blocks = ["10.128.0.12/32"]  # Только Prometheus (vm3)
-    port           = 4040
-  }
-
+  # Доступ к веб-серверам (приватная подсеть)
   egress {
-    description    = "Allow all outbound"
-    protocol       = "ANY"
-    v4_cidr_blocks = ["0.0.0.0/0"]
+    description    = "Allow to web servers"
+    protocol       = "TCP"
+    v4_cidr_blocks = [yandex_vpc_subnet.private.v4_cidr_blocks[0]]
+    port           = 80
   }
 }
 
@@ -98,10 +298,13 @@ resource "yandex_compute_instance" "vm1" {
 
   # Network interface
   network_interface {
-    index              = 0
-    subnet_id          = yandex_vpc_subnet.foo1.id
-    nat                = false
-    security_group_ids = [yandex_vpc_security_group.default.id]
+    index     = 0
+    subnet_id = yandex_vpc_subnet.foo1.id
+    nat       = false
+    security_group_ids = [
+      yandex_vpc_security_group.private_network_sg.id,
+      yandex_vpc_security_group.web_servers_sg.id
+    ]
   }
   metadata = {
     user-data          = file("./cloud-init.yml") # Используйте file() для чтения файла
@@ -131,10 +334,13 @@ resource "yandex_compute_instance" "vm2" {
 
   # Network interface
   network_interface {
-    index              = 0
-    subnet_id          = yandex_vpc_subnet.foo2.id
-    nat                = false
-    security_group_ids = [yandex_vpc_security_group.default.id]
+    index     = 0
+    subnet_id = yandex_vpc_subnet.foo2.id
+    nat       = false
+    security_group_ids = [
+      yandex_vpc_security_group.private_network_sg.id,
+      yandex_vpc_security_group.web_servers_sg.id
+    ]
   }
   metadata = {
     user-data          = file("./cloud-init.yml") # Используйте file() для чтения файла
@@ -164,10 +370,13 @@ resource "yandex_compute_instance" "vm3" {
 
   # Network interface
   network_interface {
-    index              = 0
-    subnet_id          = yandex_vpc_subnet.foo2.id
-    nat                = false
-    security_group_ids = [yandex_vpc_security_group.default.id]
+    index     = 0
+    subnet_id = yandex_vpc_subnet.foo2.id
+    nat       = false
+    security_group_ids = [
+      yandex_vpc_security_group.private_network_sg.id,
+      yandex_vpc_security_group.prometheus_sg.id
+    ]
   }
   metadata = {
     user-data          = file("./cloud-init.yml") # Используйте file() для чтения файла
@@ -200,7 +409,7 @@ resource "yandex_compute_instance" "vm4" {
     index              = 0
     subnet_id          = yandex_vpc_subnet.foo3.id
     nat                = true
-    security_group_ids = [yandex_vpc_security_group.default.id]
+    security_group_ids = [yandex_vpc_security_group.grafana_sg.id]
   }
   metadata = {
     user-data          = file("./cloud-init.yml")
@@ -230,10 +439,13 @@ resource "yandex_compute_instance" "vm5" {
 
   # Network interface
   network_interface {
-    index              = 0
-    subnet_id          = yandex_vpc_subnet.foo2.id
-    nat                = false
-    security_group_ids = [yandex_vpc_security_group.default.id]
+    index     = 0
+    subnet_id = yandex_vpc_subnet.foo2.id
+    nat       = false
+    security_group_ids = [
+      yandex_vpc_security_group.private_network_sg.id,
+      yandex_vpc_security_group.elasticsearch_sg.id
+    ]
   }
   metadata = {
     user-data          = file("./cloud-init.yml")
@@ -266,7 +478,7 @@ resource "yandex_compute_instance" "vm6" {
     index              = 0
     subnet_id          = yandex_vpc_subnet.foo3.id
     nat                = true
-    security_group_ids = [yandex_vpc_security_group.default.id]
+    security_group_ids = [yandex_vpc_security_group.kibana_sg.id]
   }
   metadata = {
     user-data          = file("./cloud-init.yml")
@@ -275,6 +487,35 @@ resource "yandex_compute_instance" "vm6" {
   }
 }
 
+#VM7 - Bastion
+
+resource "yandex_compute_instance" "bastion" {
+  name        = "bastion-host"
+  platform_id = "standard-v3"
+  zone        = "ru-central1-c"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8lcd9f54ldmonh1d72"
+      size     = 10
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.foo3.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.bastion_sg.id]
+  }
+
+  metadata = {
+    ssh-keys = "vboxuser:${file("~/.ssh/id_ed25519.pub")}"
+  }
+}
 
 # Target Group
 
@@ -377,19 +618,19 @@ resource "yandex_alb_load_balancer" "web_alb" {
 # Backup
 
 resource "yandex_compute_snapshot_schedule" "daily_backup" {
-  name = "daily-vm-backups"
+  name        = "daily-vm-backups"
   description = "Daily automated snapshots with 1-week retention"
 
   # Расписание: каждый день в 01:00 ночи
   schedule_policy {
-    expression = "0 1 * * *"  # cron формат
+    expression = "0 1 * * *" # cron формат
   }
 
- 
-  retention_period = "168h" 
-  
 
-  
+  retention_period = "168h"
+
+
+
   snapshot_spec {
     description = "Automatic daily backup of VM disks"
   }
@@ -398,14 +639,14 @@ resource "yandex_compute_snapshot_schedule" "daily_backup" {
     # Web servers
     yandex_compute_instance.vm1.boot_disk[0].disk_id,
     yandex_compute_instance.vm2.boot_disk[0].disk_id,
-    
+
     # Monitoring stack
-    yandex_compute_instance.vm3.boot_disk[0].disk_id,  # Prometheus
-    yandex_compute_instance.vm4.boot_disk[0].disk_id,  # Grafana
-    
+    yandex_compute_instance.vm3.boot_disk[0].disk_id, # Prometheus
+    yandex_compute_instance.vm4.boot_disk[0].disk_id, # Grafana
+
     # ELK stack
-    yandex_compute_instance.vm5.boot_disk[0].disk_id,  # Elasticsearch
-    yandex_compute_instance.vm6.boot_disk[0].disk_id,  # Kibana
+    yandex_compute_instance.vm5.boot_disk[0].disk_id, # Elasticsearch
+    yandex_compute_instance.vm6.boot_disk[0].disk_id, # Kibana
   ]
 
   # Зависимости - сначала должны быть созданы все VM
--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,30 @@
+# outputs.tf
 output "vm1_internal_ip" {
-  value = yandex_compute_instance.vm1.network_interface[0].ip_address
+  description = "Internal IP address of Web Server 1"
+  value       = yandex_compute_instance.vm1.network_interface[0].ip_address
 }
 
 output "vm2_internal_ip" {
-  value = yandex_compute_instance.vm2.network_interface[0].ip_address
+  description = "Internal IP address of Web Server 2"
+  value       = yandex_compute_instance.vm2.network_interface[0].ip_address
 }
 
 output "vm3_internal_ip" {
-  value = yandex_compute_instance.vm3.network_interface[0].ip_address
+  description = "Internal IP address of Prometheus server"
+  value       = yandex_compute_instance.vm3.network_interface[0].ip_address
 }
 
 output "vm4_internal_ip" {
-  value = yandex_compute_instance.vm4.network_interface[0].ip_address
+  description = "Internal IP address of Grafana server"
+  value       = yandex_compute_instance.vm4.network_interface[0].ip_address
 }
 
 output "vm5_internal_ip" {
-  value = yandex_compute_instance.vm5.network_interface[0].ip_address
+  description = "Internal IP address of Elasticsearch server"
+  value       = yandex_compute_instance.vm5.network_interface[0].ip_address
 }
+
 output "vm6_internal_ip" {
-  value = yandex_compute_instance.vm6.network_interface[0].ip_address
+  description = "Internal IP address of Kibana server"
+  value       = yandex_compute_instance.vm6.network_interface[0].ip_address
 }
